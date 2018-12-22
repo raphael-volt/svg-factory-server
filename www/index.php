18ee2eab5bb9efbc62d2e6dbf50dc532125c2422
raphael-volt <raphael@ketmie.com>
2018-12-22 20:52:54 +0100

    bug fix authorization for prod and dev

--- www/index.php
+++ www/index.php
@@ -5,10 +5,15 @@ error_reporting(E_ALL);
 
 function outputJSON($value)
 {
-    echo json_encode($value, JSON_PRETTY_PRINT);
+    echo json_encode($value, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
     exit();
 }
+function exit200() {
+    header("HTTP/1.1 200 OK");
+    exit;
+}
 $method = null;
+
 if (isset($_SERVER['REQUEST_METHOD'])) {
     $method = $_SERVER['REQUEST_METHOD'];
     switch ($method) {
@@ -25,20 +30,49 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
     }
 }
 
+if ($method == 'OPTIONS' || $method == 'HEAD') {
+    exit200();
+}
+
 if ($method == null) {
     header("HTTP/1.1 405 Method Not  Allowed");
     exit();
 }
+if (isset($_GET["XDEBUG_SESSION_START"]) || isset($_GET["XDEBUG_SESSION_STOP_NO_EXEC"])) {
+    exit200();
+}
+if (isset($_GET["login"])) {
+    exit200();
+}
+$headers = apache_request_headers();
+$authorized = false;
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X-Api-Session-Id");
-if ($method == 'OPTIONS' || $method == 'HEAD') {
-    header("HTTP/1.1 200 OK");
+if (isset($headers['X-Auth'])) {
+    $data = $headers['X-Auth'];
+    if (preg_match('/Basic\s+(.*)$/i', $data, $matches)) {
+        list ($name, $password) = explode(':', base64_decode($matches[1]));
+        $authorized = ($name == "user" && $password == "password");
+    }
+}
+
+if (! $authorized) {
+    header("HTTP/1.1 401 Unauthorized");
     exit();
 }
 
-$pdo = new PDO("mysql:host=svg-db; dbname=symbols_db;", "dbuser", "dbuserpwd", array(
+$prod = false;
+if ($prod) {
+    define('DB_HOST', 'ketmieuser.mysql.db');
+    define('DB_LOGIN', 'ketmieuser');
+    define('DB_PASSWORD', 'k3tM1aDm1n');
+    define('DB_NAME', 'ketmieuser');
+} else {
+    define('DB_HOST', 'svg-db');
+    define('DB_LOGIN', 'dbuser');
+    define('DB_PASSWORD', 'dbuserpwd');
+    define('DB_NAME', 'symbols_db');
+}
+$pdo = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME . ";", DB_LOGIN, DB_PASSWORD, array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"
 ));
 
@@ -47,8 +81,8 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if ($method == "POST" || $method == "PUT") {
     $json = file_get_contents('php://input');
     $sym = json_decode($json);
-    if(! property_exists($sym, "name"))
-       $sym->name = "";
+    if (! property_exists($sym, "name"))
+        $sym->name = "";
     if ($method == "POST") {
         $q = "INSERT INTO symbol (name, data, width, height, holes, pathLength) VALUES (?, ?, ?, ?, ?, ?);";
         $s = $pdo->prepare($q);
@@ -59,7 +93,7 @@ if ($method == "POST" || $method == "PUT") {
         $s->bindValue(5, $sym->holes);
         $s->bindValue(6, $sym->pathLength);
         try {
-            $s->execute();            
+            $s->execute();
         } catch (PDOException $e) {
             // something wrong
             $m = $e->getMessage();
